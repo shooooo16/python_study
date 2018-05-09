dd7af7dcf060a8e74f7b03f3e729610a5e15c5b1
APLIEOMVACEREPC\sho <erecavamoeilpa@gmail.com>
2018-05-09 17:50:48 +0900

    様々な演算

---
+++ myapp4.py
@@ -0,0 +1,21 @@
+# + - * / // % **
+
+x = 10
+print(x /3) #3.33..
+print(x //3)#3
+print(x %3) #1
+print(x *3) #30
+print(x **3)#1000
+
+y = 4
+y +=12
+print(y) #16
+
+#and or not
+print(True and False) #False
+print(True or False) #True
+print(not True) #False
+
+# + *
+print("hello" + "world") #hellworld
+print("hello " * 3) #hello hello hello 
