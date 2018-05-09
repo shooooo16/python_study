774db860585d5b09489d8bde3b522837d41fd306
APLIEOMVACEREPC\sho <erecavamoeilpa@gmail.com>
2018-05-09 14:07:58 +0900

    様々なデータ

---
+++ myapp3.py
@@ -0,0 +1,19 @@
+# 文字列
+s = "he\nllo wo\trld"
+html = """<html>
+<body><body>
+</html>"""
+print(s)
+print(html)
+
+#整数
+i = 10
+print(i+2)
+
+#浮動小数点
+f = 23.4
+print(f)
+
+#論理値
+flag =True
+print(flag)
