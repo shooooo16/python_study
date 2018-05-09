b499ed87f1a7b4009489367093c4c1b3783b8aec
APLIEOMVACEREPC\sho <erecavamoeilpa@gmail.com>
2018-05-09 18:05:10 +0900

    文字列に値を埋め込む

---
+++ myapp5.py
@@ -0,0 +1,7 @@
+name = "imai"
+score = 52.8
+
+print("name: %s,score: %f" % (name,score))
+print("name: %-10s,score: %-10.3f" % (name,score))
+print("name: {0},score: {1}" .format(name,score))
+print("name: {0:<10s},score: {1:<10.2f}" .format(name,score))
