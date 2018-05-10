f437306dda707f18baf49ca987292edd3e106b20
APLIEOMVACEREPC\sho <erecavamoeilpa@gmail.com>
2018-05-10 13:24:26 +0900

    for文

---
+++ myapp8.py
@@ -0,0 +1,15 @@
+# for文
+
+# for 変数 in データの集合
+# 処理
+
+for i in range(0,10):
+    print(i)
+else:
+    print("for文終わり")
+    
+for i in range(0,10):
+    if i == 5:
+         print("iが5になったため終了")
+         break
+    print(i)
